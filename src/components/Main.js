b70cd2d5c551fd99abf8218d7cbb3e02b167b483
junie87 <arjun@1upvc.com>
2020-12-30 23:12:25 -0600

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,24 +33,10 @@ class Main extends React.Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+            Build
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+            Coming Soon
           </p>
           {close}
         </article>
@@ -68,17 +54,17 @@ class Main extends React.Component {
           </span>
           <p>
             Stake with Us
-
+            </p>
+            <p>
             We beleive in a decentralized future of commerce and are supporters
             of the <a href='https://cardano.org/'>Cardano Project</a>.  Staking provides an opportunity for securing the Cardano
             network by processing data and keep the network trusted.
 
-
-
-          </p>
-          <p>
+            </p>
+            <p>
             Stake with a Partner
-
+            </p>
+            <p>
             Interested in working with an larger partner to expand your network?  We've got you covered.
             Check out <a href='https://staked.us/'>Staked.US</a>.
 
