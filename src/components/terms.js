94573e19f218b3005c50adb44701ca0ba5f7aba7
junie87 <arjun@1upvc.com>
2021-01-09 09:26:49 -0600

    Update terms.js

--- src/components/terms.js
+++ src/components/terms.js
@@ -50,3 +50,18 @@ class Main extends React.Component {
           </p>
           {close}
         </article>
+        </div>
+      )
+    }
+  }
+
+  Main.propTypes = {
+    route: PropTypes.object,
+    article: PropTypes.string,
+    articleTimeout: PropTypes.bool,
+    onCloseArticle: PropTypes.func,
+    timeout: PropTypes.bool,
+    setWrapperRef: PropTypes.func.isRequired,
+  }
+
+  export default Main
