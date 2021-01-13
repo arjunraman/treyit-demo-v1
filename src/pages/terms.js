d57dcd645c3877efabfc903929295acc458e869c
junie87 <arjun@1upvc.com>
2021-01-13 08:13:48 -0600

    Update terms.js

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -6,10 +6,25 @@ import Layout from '../components/layout'
 const SecondPage = () => (
   <Layout>
     <h1>Terms of Use</h1>
+    <Link to="/">Return to homepage</Link>
     <p>Date of Last Update: January 12, 2021</p>
     <h2>Introduction</h2>
-    <p>TBD</p>
-    <Link to="/">Return to homepage</Link>
+    <p>These terms of use (these “Terms”) cover your use of and access to https://1upvc.com and its associated domains (the “Website”). These Terms form part of a binding contract between you and 1UP Ventures, LLC (“we,” “us,” “our,” or “1UPVC”). By using or accessing the Website, you are agreeing to both these Terms and our Privacy Policy (collectively, this “Agreement”). If you use or access the Website in your capacity as a representative or employee of an organization, you agree to this Agreement on that organization’s behalf and you represent and warrant that you have the authority do so. References to “you,” “your” or similar terms in this Agreement refer to the individual or organization agreeing to this Agreement.
+    PLEASE READ THIS AGREEMENT CAREFULLY. IT INCLUDES IMPORTANT INFORMATION ABOUT YOUR LEGAL RIGHTS AND OBLIGATIONS. IF YOU DO NOT ACCEPT THIS AGREEMENT, WE DO NOT GRANT YOU ANY LICENSE OR USE RIGHTS HEREUNDER, AND YOU MAY NOT USE OR ACCESS THE WEBSITE.</p>
+    <h2>Eligibility Requirements</h2>
+    <h2>Privacy</h2>
+    <h2>Acceptable and Prohibited Use</h2>
+    <h3>Deceptive, Fraudulent, and Criminial Behavior</h3>
+    <h3>Consequences of Prohibited Use</h3>
+    <h2>Third Party Services, Sites, and Content</h2>
+    <h2>Intellectual Property</h2>
+    <h2>Linking to Website</h2>
+    <h2>Termination; Reservation of Rights</h2>
+    <h2>Disclaimer of Warranty</h2>
+    <h2>Limitation of Liability; Waiver; Release</h2>
+    <h2>Indemnification</h2>
+    <h2>Time Limitation of Claims</h2>
+    <h2>Additional Terms</h2>
   </Layout>
 )
 
