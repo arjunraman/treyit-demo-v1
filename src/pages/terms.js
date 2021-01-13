1e25b862930b485e4a302d69fec298dcefbc2cd9
junie87 <arjun@1upvc.com>
2021-01-12 20:57:10 -0600

    Create terms.js

---
+++ src/pages/terms.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Terms & Conditions</h1>
+    <p>TBD</p>
+    <Link to="/">Return to homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
