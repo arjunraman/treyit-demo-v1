d4c80ed1c353064b8641e0700c5beda5bb2b5ec2
junie87 <arjun@1upvc.com>
2021-01-12 20:14:54 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from '../components/layout'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
-import Terms from '../components/terms'
 
 
 class IndexPage extends React.Component {
