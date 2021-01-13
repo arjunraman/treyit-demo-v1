59d053d23cc003c80661df16a592dda2f0ceb44d
junie87 <arjun@1upvc.com>
2021-01-12 20:48:38 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from "gatsby"
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; 2021 1UPVC LLC.| <Link to="/SecondPage/">Terms</Link></p>
+        <p className="copyright">&copy; 2021 1UPVC LLC.| <Link to="/SecondPage/">Contact</Link></p>
     </footer>
 )
 
