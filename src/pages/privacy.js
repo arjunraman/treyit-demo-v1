fc48f1a766ac116c4cc13e646edd995effeb78fa
junie87 <arjun@1upvc.com>
2021-01-12 20:57:12 -0600

    Create privacy.js

---
+++ src/pages/privacy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Privacy Policy</h1>
+    <p>TBD</p>
+    <Link to="/">Return to homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
