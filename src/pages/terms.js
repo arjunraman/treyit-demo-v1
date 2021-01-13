65b3712770c37ff9b8b42245223404ce1c9c6b17
junie87 <arjun@1upvc.com>
2021-01-13 00:03:00 -0600

    Update terms.js

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -5,7 +5,9 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Terms & Conditions</h1>
+    <h1>Terms of Use</h1>
+    <p>Date of Last Update: January 12, 2021</p>
+    <h2>Introduction</h2>
     <p>TBD</p>
     <Link to="/">Return to homepage</Link>
   </Layout>
