7ba222b0a114ecb4c83958dfbef15746b34c5754
junie87 <arjun@1upvc.com>
2021-01-16 13:14:46 -0600

    Update terms.js

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -12,11 +12,86 @@ const SecondPage = () => (
     <p>These terms of use (these “Terms”) cover your use of and access to https://1upvc.com and its associated domains (the “Website”). These Terms form part of a binding contract between you and 1UP Ventures, LLC (“we,” “us,” “our,” or “1UPVC”). By using or accessing the Website, you are agreeing to both these Terms and our Privacy Policy (collectively, this “Agreement”). If you use or access the Website in your capacity as a representative or employee of an organization, you agree to this Agreement on that organization’s behalf and you represent and warrant that you have the authority do so. References to “you,” “your” or similar terms in this Agreement refer to the individual or organization agreeing to this Agreement.
     PLEASE READ THIS AGREEMENT CAREFULLY. IT INCLUDES IMPORTANT INFORMATION ABOUT YOUR LEGAL RIGHTS AND OBLIGATIONS. IF YOU DO NOT ACCEPT THIS AGREEMENT, WE DO NOT GRANT YOU ANY LICENSE OR USE RIGHTS HEREUNDER, AND YOU MAY NOT USE OR ACCESS THE WEBSITE.</p>
     <h2>Eligibility Requirements</h2>
+    <h3>Minimum Age</h3>
+    <p>You must be 18 years of age or older to use the Website. If you are under the age of 18, please discontinue use of the Website immediately.
+    1UPVC reserves the right to ask for proof of age and to withhold or terminate your use of the Website if you are below the speciﬁed age requirement.</p>
+    <h3>Legal Capacity</h3>
+    <p>You represent and warrant that you have the right, authority and capacity to enter into this Agreement and to abide by all of the terms and conditions of this Agreement.
+    If you are representing a corporation or other legal business entity, you hereby represent and warrant that such entity is validly formed and existing under the laws of its jurisdiction and that you are duly authorized as its agent to bind it to this Agreement. </p>
+    <h3>Geographic Limitations</h3>
+    <p>Accessing or using the Website may be prohibited or restricted in certain countries. If you access or use the Website from outside of the United States, you are responsible for complying with the laws and regulations of the country, territory or jurisdiction from which you access or use the Website. By accessing or using the Website, you represent and warrant that you are not located in, under the control of, or a national or resident of, as the case may be, any person, organization or country which the United States has (i) identified as a “Specially Designated National,” (ii) placed on the Commerce Department’s Table of Deny Orders or (iii) specified as the target of economic sanctions. </p>
+
     <h2>Privacy</h2>
+    <h3>Personal Information</h3>
+    <p>In some cases, you may be required to upload certain personal information through the Website. Such information includes, without limitation: name, email address, phone number and business address. When you provide us with such information, you agree that it will be accurate, complete and up to date. We may need to use this information to contact you regarding your use of the Website. In addition, we may automatically collect certain information about you such as your IP address and the duration of your visit to the Website. We do our best to handle your personal information in a secure and responsible manner, as detailed in our Privacy Policy.</p>
     <h2>Acceptable and Prohibited Use</h2>
+    <h3>Legal Use</h3>
+    <p>You represent and warrant that your use of the Website is not contrary to law, including, without limitation, applicable privacy laws, export or import controls and sanctions. You may not use the Website where prohibited by law.</p>
+    <h3>Unowned or Licensed Content</h3>
+    <p>Certain content on the Website may be protected by intellectual property, trade secret or other rights. You shall not copy, upload, download or share content unless you have the right to do so.</p>
+    <h3>Interfering with the Website</h3>
+    <p>You agree not to interfere with or disrupt the Website or servers, networks or devices connected to the Website, including by transmitting any worms, viruses, spyware, malware or any other code of a destructive or disruptive nature. You shall not inject content or code or otherwise alter or interfere with the way any part of the Website is rendered or displayed in a user’s browser or device. In addition, you agree not to:
+
+    (1)	probe, scan or test the vulnerability of any system or network;
+
+    (2)	breach or otherwise bypass any security or authentication measures;
+
+    (3)	access, tamper with or use non-public areas or parts of the Website, or shared areas of the Website that 1UPVC has not invited you to access;
+
+    (4)	take apart, decompile or reverse engineer any part of the Website in an effort to access things such as source code or algorithms;
+
+    (5)	access or search the Website by any means (for example, scraping, spidering or crawling) other than our publicly supported interfaces; and
+
+    (6)	take action that imposes an unreasonable load on our infrastructure or that of our third party providers (where 1UPVC reserves the right to determine what is reasonable or unreasonable).
+
+    </p>
+
     <h3>Deceptive, Fraudulent, and Criminial Behavior</h3>
+    When using or accessing the Website, you agree not to:
+  <p>
+    (1)	act in a way that is false, fraudulent, inaccurate or deceiving;
+
+    (2)	impersonate another person, company or entity;
+
+    (3)	engage in misleading or unethical marketing or advertising;
+
+    (4)	infringe or misappropriate anyone’s copyright, trademark or other intellectual property rights or to violate anyone’s privacy or publicity rights.
+
+    (5)	threaten, harass or abuse any individuals;
+
+    (6)	incite violence;
+
+    (7)	publish sexually explicit or obscene material;
+
+    (8)	condone or promote self-harm;
+
+    (9)	condone or promote violence against any person or group based on race, ethnicity, nationality, religion, gender, gender identity, sexual preference, age or disability; or
+
+    (10)	violate any laws through your use of the Website, including, without limitation, all local laws regarding online conduct and acceptable content.
+    </p>
+
     <h3>Consequences of Prohibited Use</h3>
+    <p>
+    If we conclude, at our sole discretion, that you have misused the Website, we may take action against you. We do our best to ensure fair outcomes, but in all cases, we reserve the right to remove any content or prohibit your use of the Website without liability or notice to you, at any time and for any reason (except where prohibited by applicable law).
+
+    You may report violations of these guidelines directly to 1UPVC by emailing privacy@1UPVC.com.
+    </p>
+
     <h2>Third Party Services, Sites, and Content</h2>
+
+    <h3>Third party Services</h3>
+    <p>
+    The Website is integrated with various third party services, websites, and applications (collectively, “Third Party Services”) that may allow you to access their content and products. Examples of Third Party Services include social media platforms, news websites and other service providers. Third Party Services may have their own terms and policies, and your use of them will be governed by those terms and policies. We do not control Third Party Services, and we are not liable for the actions of any Third Party Services or for any transaction you may enter into with them. Your security when using Third Party Services is your responsibility. You also agree that we may, at any time and in our sole discretion, and without any notice to you, suspend, disable access to or remove any Third Party Services. We are not liable to you for any such suspension, disabling or removal, including without limitation for any loss of profits, revenue, data, goodwill or other intangible losses you may experience as a result (except where prohibited by applicable law).
+    </p>
+    <h3>Third party Websites</h3>
+    <p>
+    The Website may contain links to third party sites. When you access third party sites, you do so at your own risk. We do not control and are not liable for any part of those sites or any actions of such third parties.
+    </p>
+    <h3>Third party content</h3>
+    <p>
+    The Website may contain third party content: (1) that is offensive or objectionable; (2) that contains errors; (3) that violates intellectual property, trade secret, privacy, publicity or other rights or the good name of you or third parties; (4) that is harmful to your or others’ computers or networks; (5) that is unlawful or illegal; or (6) the downloading, copying or use of which is subject to additional terms and policies of third parties or is protected by intellectual property, trade secret, privacy or other laws. We do not endorse third party content, or represent that we believe such content to be accurate, useful, lawful or non-harmful. You are responsible for taking precautions to protect yourself, and your computer or network, from third party content accessed via the Website.
+    </p>
+
     <h2>Intellectual Property</h2>
     <h2>Linking to Website</h2>
     <h2>Termination; Reservation of Rights</h2>
