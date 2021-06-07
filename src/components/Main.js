ee337a56cab8cfbd38519f85522b00ef149b942b
junie87 <arjun@1upvc.com>
2021-06-06 20:32:12 -0500

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -57,7 +57,7 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">TBD/h2>
+          <h2 className="major">TBD</h2>
           <span className="image main">
             <img src={pic02} alt="" />
           </span>
