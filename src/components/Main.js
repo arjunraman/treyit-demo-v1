9efe04a3f83491cd595a407f07bde5401bfd413d
junie87 <arjun@1upvc.com>
2021-06-29 21:02:12 -0500

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -136,7 +136,7 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Request</h2>
+          <h2 className="major">Search</h2>
           <form method="post" action="#" netlify-honeypot="bot-field" data-netlify="true" name="contact" onSubmit='submit'>
             <input type="hidden" name="bot-field" />
             <input type="hidden" name="form-name" value="contact" />
